434bb9c410571410f3aea9526cd683e6cc25777a
Alex Minu <alexandru_minu@yahoo.com>
2018-10-14 09:34:54 +0300

    player movement input for android and ios

--- SpaceBlubber/Assets/Scripts/PlayerMovement.cs
+++ SpaceBlubber/Assets/Scripts/PlayerMovement.cs
@@ -17,6 +17,8 @@ public class PlayerMovement : MonoBehaviour {
 
     private void Update()
     {
+        #region Editor Controlls
+#if UNITY_EDITOR
         if (Input.GetMouseButtonDown(0))
         {
             touchPosition = Input.mousePosition;
@@ -26,8 +28,7 @@ public class PlayerMovement : MonoBehaviour {
             Vector2 deltaSwipe = touchPosition - Input.mousePosition;
             float swipeXAbs = Mathf.Abs(deltaSwipe.x);
             float swipeYAbs = Mathf.Abs(deltaSwipe.y);
-
-            if(swipeXAbs > swipeDeadZone || swipeYAbs > swipeDeadZone)
+            if (swipeXAbs > swipeDeadZone || swipeYAbs > swipeDeadZone)
             {
                 if (swipeXAbs >= swipeYAbs)//RIGHT or LEFT
                 {
@@ -39,5 +40,63 @@ public class PlayerMovement : MonoBehaviour {
                 }
             }
         }
+#endif
+        #endregion
+
+#if UNITY_ANDROID
+        if (Input.touchCount == 1)
+        {
+            if(Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                touchPosition = Input.GetTouch(0).position;
+            }
+            if(Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                Vector2 deltaSwipe = (Vector2)touchPosition - Input.GetTouch(0).position;
+                float swipeXAbs = Mathf.Abs(deltaSwipe.x);
+                float swipeYAbs = Mathf.Abs(deltaSwipe.y);
+
+                if (swipeXAbs > swipeDeadZone || swipeYAbs > swipeDeadZone)
+                {
+                    if (swipeXAbs >= swipeYAbs)//RIGHT or LEFT
+                    {
+                        rigidBody.AddForce(transform.right * (deltaSwipe.x > 0f ? 1 : -1) * moveForce, ForceMode.Impulse);
+                    }
+                    else//UP or DOWN
+                    {
+                        rigidBody.AddForce(transform.up * (deltaSwipe.y > 0f ? 1 : -1) * moveForce, ForceMode.Impulse);
+                    }
+                }
+            }
+        }
+#endif
+
+#if UNITY_IOS
+        if (Input.touchCount == 1)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                touchPosition = Input.GetTouch(0).position;
+            }
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                Vector2 deltaSwipe = (Vector2)touchPosition - Input.GetTouch(0).position;
+                float swipeXAbs = Mathf.Abs(deltaSwipe.x);
+                float swipeYAbs = Mathf.Abs(deltaSwipe.y);
+
+                if (swipeXAbs > swipeDeadZone || swipeYAbs > swipeDeadZone)
+                {
+                    if (swipeXAbs >= swipeYAbs)//RIGHT or LEFT
+                    {
+                        rigidBody.AddForce(transform.right * (deltaSwipe.x > 0f ? 1 : -1) * moveForce, ForceMode.Impulse);
+                    }
+                    else//UP or DOWN
+                    {
+                        rigidBody.AddForce(transform.up * (deltaSwipe.y > 0f ? 1 : -1) * moveForce, ForceMode.Impulse);
+                    }
+                }
+            }
+        }
+#endif
     }
 }
