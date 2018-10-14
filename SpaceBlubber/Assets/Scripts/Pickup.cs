d4a5dab74ee21206df20d872e90eb5f4b072f815
Alex Minu <alexandru_minu@yahoo.com>
2018-10-14 19:25:39 +0300

    cinemachine

--- SpaceBlubber/Assets/Scripts/Pickup.cs
+++ SpaceBlubber/Assets/Scripts/Pickup.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pickup : MonoBehaviour {
+    public float massValue = 5f;
+
     private void OnTriggerEnter(Collider other)
     {
-        
+        PlayerMovement.instance.IncreaseMass(massValue);
+        Destroy(gameObject);
     }
 }
--- SpaceBlubber/Assets/Scripts/PlayerMovement.cs
+++ SpaceBlubber/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,11 @@ public class PlayerMovement : MonoBehaviour {
     public float moveForce = 1f;
     public bool invertedControls = false;
 
+    public float massMoveCostPercent = 10f;
+    public float criticalMass = 20f;
+
+    public static PlayerMovement instance;
+
     Vector3 touchPosition;
     float swipeDeadZone = 50f;
     Rigidbody rigidBody;
@@ -14,12 +19,13 @@ public class PlayerMovement : MonoBehaviour {
     private void Awake()
     {
         rigidBody = GetComponent<Rigidbody>();
+        instance = this;
     }
 
     private void Update()
     {
-        #region Editor Controlls
-#if UNITY_EDITOR
+        #region Editor or Standalone Controlls
+#if UNITY_EDITOR || UNITY_STANDALONE
         if (Input.GetMouseButtonDown(0))
         {
             touchPosition = Input.mousePosition;
@@ -44,7 +50,7 @@ public class PlayerMovement : MonoBehaviour {
 #endif
         #endregion
 
-#if UNITY_ANDROID
+#if UNITY_ANDROID || UNITY_IOS
         if (Input.touchCount == 1)
         {
             if(Input.GetTouch(0).phase == TouchPhase.Began)
@@ -71,38 +77,21 @@ public class PlayerMovement : MonoBehaviour {
             }
         }
 #endif
-
-#if UNITY_IOS
-        if (Input.touchCount == 1)
-        {
-            if (Input.GetTouch(0).phase == TouchPhase.Began)
-            {
-                touchPosition = Input.GetTouch(0).position;
-            }
-            if (Input.GetTouch(0).phase == TouchPhase.Ended)
-            {
-                Vector2 deltaSwipe = (Vector2)touchPosition - Input.GetTouch(0).position;
-                float swipeXAbs = Mathf.Abs(deltaSwipe.x);
-                float swipeYAbs = Mathf.Abs(deltaSwipe.y);
-
-                if (swipeXAbs > swipeDeadZone || swipeYAbs > swipeDeadZone)
-                {
-                    if (swipeXAbs >= swipeYAbs)//RIGHT or LEFT
-                    {
-                        Move(transform.right * (deltaSwipe.x > 0f ? 1 : -1));
-                    }
-                    else//UP or DOWN
-                    {
-                        Move(transform.up * (deltaSwipe.y > 0f ? 1 : -1));
-                    }
-                }
-            }
-        }
-#endif
     }
 
     void Move(Vector3 direction)
     {
         rigidBody.AddForce(direction * moveForce * (invertedControls ? -1 : 1), ForceMode.VelocityChange);
+
+        rigidBody.mass *= 1f - massMoveCostPercent / 100f;
+        if(rigidBody.mass < criticalMass)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void IncreaseMass(float ammount)
+    {
+        rigidBody.mass += ammount;
     }
 }
